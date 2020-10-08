6a10e50642bc8bf6183a75764f9a63616251f316
meghanRedShift <70180448+meghanRedShift@users.noreply.github.com>
2020-10-08 16:22:49 -0700

    Revert "I messed up all kinds of stuff, going back to a previous version"
    This reverts commit 8261ee7b5d67d87490b48f826ed3e1e3b531389c.

--- Module Testing.py
+++ Module Testing.py
@@ -1,3 +1,7 @@
+from flask import Flask, request, make_response
+import sys
+
+app = Flask(__name__)
 
 #Import relevant libraries
 from ics import Calendar, Event
@@ -7,10 +11,11 @@ import datetime
 hrs = [15,16,17,17,17,18,18,19,20,20,21,22,22]
 mins = [45,00,00,15,30,30,45,30,30,45,00,00,15]
 dur = [15,60,15,15,60,15,45,60,15,15,60,15,30]
-#List of year, month, and date of holidays on the school calendar
-hy = [2020,2020,2020]
-hm = [10,11,11]
-hd = [12,3,11]
+yk = datetime.date(2020,9, 28)
+mt = datetime.date(2020,10, 12)
+hy = [2020,2020]
+hm = [9, 10]
+hd = [28, 12]
 class cal:
     def __init__(self, courseNames, options, y, m, d):
         self.cal= Calendar()
@@ -20,11 +25,8 @@ class cal:
         self.m = m
         self.d = d
 
-        #creates a 4 day week in calendar using the provided list of course names and week start date
+    #creates a 4 day week in calendar using the provided list of course names and week start date
     def create4DayWeek(self):
-        #Checks if the checkbox for each option has been selected and
-        #sets the name for that option accordingly. The name "" will
-        #not create an event
         if self.options[0]:
             mAdvisory = 'Advisory/Community Flex Time'
         else:
@@ -43,10 +45,10 @@ class cal:
             conferencing = ""
         
         #List of names for each event depending on whether the day is even or odd schedule
-        oddNames = [mAdvisory, self.courseNames[0], self.courseNames[0] + " Flex", breaks,
+        evenNames = [mAdvisory, self.courseNames[0], self.courseNames[0] + " Flex", breaks,
                      self.courseNames[2], self.courseNames[2] + " Flex", lunch, self.courseNames[4], self.courseNames[4] +
                      " Flex", breaks, self.courseNames[6], self.courseNames[6] + " Flex", conferencing]
-        evenNames = [mAdvisory, self.courseNames[1], self.courseNames[1] + " Flex", breaks,
+        oddNames = [mAdvisory, self.courseNames[1], self.courseNames[1] + " Flex", breaks,
                      self.courseNames[3], self.courseNames[3] + " Flex", lunch, self.courseNames[5], self.courseNames[5] +
                      " Flex", breaks, self.courseNames[7], self.courseNames[7] + " Flex", conferencing]
         #The outer loop makes this run for each day of the week
@@ -65,39 +67,111 @@ class cal:
                     #Adjusts date using z. This is in a separate statement so that the
                     #added day makes the month roll over if necessary
                     beg +=datetime.timedelta(days=z-1)
-                    
-                    print(beg)
-                    #Loops through each holiday date
                     for i in range(len(hy)):
-                        #Creates a date object for each holiday date
                         checkHol = datetime.date(hy[i], hm[i], hd[i])
-                        holiday = False
-                        #Compares the current date to the holiday and skips
-                        #to the next day if the current one is a holiday
+                        print("Happy Holidays!")
                         if beg.date() == checkHol:
-                            holiday = True
-                            print("Holiday")
+                            print (beg.date())
                             beg+=datetime.timedelta(days=1)
-                            print(beg)
-                            print("hol")
                             self.d = beg.day
                             self.m = beg.month
                             self.y = beg.year
-                            print(beg)
                     #Creates calendar event
                     e=Event(name=names[x], begin=beg, duration={"minutes":dur[x]})
                     #Adds event to calendar
                     self.cal.events.add(e)
                     self.cal.events
-                    if holiday == True:
-                        beg-=datetime.timedelta(days=1)
-                        self.d = beg.day
-                        self.m = beg.month
-                        self.y = beg.year
-                        print(beg)
+            
+        #Prints calendar file to console
+        print(str(self.cal))
+        #Saves calendar file to console
+        #I just keep writing to the same calendar. Haven't had issues with this but it could possibly cause issues.
+        open('my.ics', 'w').writelines(self.cal)
+        beg+=datetime.timedelta(days=3)
+        self.d = beg.day
+        self.m = beg.month
+        self.y = beg.year
 
+    #creates a 4 day week in calendar using the provided list of course names and week start date
+    def create5DayWeek(self):
+        if self.options[0]:
+            mAdvisory = 'Advisory/Community Flex Time'
+        else:
+            mAdvisory = ""
+        if self.options[1]:
+            breaks='Break'
+        else:
+            breaks=""
+        if self.options[2]:
+            lunch="Lunch"
+        else:
+            lunch=""
+        if self.options[3]:
+            conferencing = 'Conferencing'
+        else:
+            conferencing = ""
+        #List of modified durations for long days on 5 day schedule
+        dur5 = [15,60,15,15,60,15,45,60,15,15,105]
+        #List of names for each event depending on the day of the week
+        mNames = [mAdvisory, self.courseNames[0], self.courseNames[0] + " Flex", breaks,
+                     self.courseNames[2], self.courseNames[2] + " Flex", "Lunch", self.courseNames[4], self.courseNames[4] +
+                     " Flex", breaks, self.courseNames[6], self.courseNames[6] + " Flex", "Conferencing"]
+        tNames = [mAdvisory, self.courseNames[1], self.courseNames[1] + " Flex", breaks,
+                     self.courseNames[5], self.courseNames[5] + " Flex", "Lunch", self.courseNames[7], self.courseNames[7] +
+                     " Flex", breaks, "Conferencing"]
+        wNames = [mAdvisory, self.courseNames[3], self.courseNames[3] + " Flex", breaks,
+                     self.courseNames[4], self.courseNames[4] + " Flex", "Lunch", self.courseNames[6], self.courseNames[6] +
+                     " Flex", breaks, "Conferencing"]
+        thNames = [mAdvisory, self.courseNames[0], self.courseNames[0] + " Flex", breaks,
+                     self.courseNames[2], self.courseNames[2] + " Flex", "Lunch", self.courseNames[5], self.courseNames[5] +
+                     " Flex", breaks, "Faculty Collaboration"]
+        fNames = [mAdvisory, self.courseNames[1], self.courseNames[1] + " Flex", breaks,
+                     self.courseNames[3], self.courseNames[3] + " Flex", "Lunch", self.courseNames[7], self.courseNames[7] +
+                     " Flex", breaks, "Conferencing"]
+        #The outer loop makes this run for each day of the week
+        for z in range(1,6):
+            #Chooses which set of names and durations to use based the day of the week
+            if (z==1):
+                names = mNames
+                duration = dur
+            elif (z==2):
+                names = tNames
+                duration = dur5
+            elif (z==3):
+                names = wNames
+                duration = dur5
+            elif (z==4):
+                names = thNames
+                duration = dur5
+            else:
+                names = fNames
+                duration = dur5
+            #The inner loop makes this run for each class in the day
+            for x in range(len(names)):
+                #if statement make it so events are only created for named courses
+                if (names[x] != "" and names[x] != " Flex"):
+                    #creates datetime for a class
+                    beg= datetime.datetime(self.y, self.m, self.d, hrs[x], mins[x])
+                    #Adjusts date using z. This is in a separate statement so that the
+                    #added day makes the month roll over if necessary
+                    beg +=datetime.timedelta(days = z-1)
+                    #Creates calendar event
+                    e=Event(name=names[x], begin=beg, duration={"minutes":duration[x]})
+                    #Adds event to calendar
+                    self.cal.events.add(e)
+                    self.cal.events
+            
+        #Prints calendar file to console
+        print(str(self.cal))
+        #Saves calendar file to console
+        #I just keep writing to the same calendar. Haven't had issues with this but it could possibly cause issues.
+        open('my.ics', 'w').writelines(self.cal)
+        beg+=datetime.timedelta(days=3)
+        self.d = beg.day
+        self.m = beg.month
+        self.y = beg.yeary= 2020
         
-courses = ["1","2","","","","","",""]
-options=["", "", "", ""]
-createCal = cal(courses, options, 2020, 10, 12)
+courses = ["","","","","","","",""]
+options=["mAdvisory", "breaks", "", ""]
+createCal = cal(courses, options, 2020, 9, 28)
 createCal.create4DayWeek()
